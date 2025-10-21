e3a8649512ffc2ef9c02a126888da2473e79099b
Shotageo <shota@sd-daisho.com>
2025-10-21 16:41:47 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from .plan_preview_upload import page as plan_preview_upload
+from .plan_preview import page as plan_preview
+from . import plan_preview_upload as plan_preview_upload
 
-__all__ = ["plan_preview_upload"]
-
+__all__ = ["plan_preview", "plan_preview_upload"]
